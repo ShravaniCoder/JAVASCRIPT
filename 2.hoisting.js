db215c7824921e1e7eab4b63df12625e6d4cbada
Shravani Chendwankar <shravanichendwankar@gmail.com>
2024-12-24 16:44:10 +0530

    hoisting, data types

---
+++ 2.hoisting.js
@@ -0,0 +1,5 @@
+// what is hoisting? Give an example.
+// in python calling a function before it is declare will throw an error
+// but in javascript, this is excute an print the output.
+// hoisting means where variable and function declarations are moved or hoisted to the top of their scope before their execution.
+// there are types of hosting is variable hoisting and function hoisting
---
+++ 3.dataTypes.js
@@ -0,0 +1,6 @@
+// What are the data types available in the javascript?
+// data types in javascript can be catogories into primitive and non-primitive
+// primitive data types are immutable means they cannot be modify
+// non-primitive data types are mutable
+// primitve data types enclosed Number, String, Boolean, Undefined, Null, Symbol, BigInt
+// 1.Number
