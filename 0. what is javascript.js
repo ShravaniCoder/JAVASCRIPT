70998a4f332e92cbcd3df927be56101c9248a341
Shravani <shravanichendwankar@gmail.com>
2025-01-28 20:23:23 +0530

    HTML HTTP

--- 0. what is javascript.js
+++ 0. what is javascript.js
@@ -1,1 +1,3 @@
 // Javascript is a high level programming language, that is widely use to create interactive effects with in web browses. A high level programming language is a human understandable language. It is commonly used for developing web & mobile applications.
+// What is HTML- Hyper Text Markup Languag, to make layout of the web page, to stucture website
+// HTTP - Hyper Text Transfer Protocol
--- 3.dataTypes.js
+++ 3.dataTypes.js
@@ -1,6 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
 // What are the data types available in the javascript?
 // data types in javascript can be catogories into primitive and non-primitive
 // primitive data types are immutable means they cannot be modify
 // non-primitive data types are mutable
 // primitve data types enclosed Number, String, Boolean, Undefined, Null, Symbol, BigInt
-// 1.Number
+// 1.Number - which represents both integer and floating-point numbers. It also includes special values like infinity, and NaN(Not-a-number). let age =25; let price=99.99; let infinite = Infinity;
+let score = 100
+let price = "400 sc"
+console.log(typeof score)
+console.log(typeof (score))
+console.log(typeof price)
+let valueInNumber = Number(price)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+let someNumber = 33
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+// 2.String - which is a sequence of characters. It can be enclosed in single quotes or double quotes. let name = "John Doe"; let address = '123 society'
+
